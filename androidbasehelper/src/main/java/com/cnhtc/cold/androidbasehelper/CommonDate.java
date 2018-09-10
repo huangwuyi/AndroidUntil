c279d0d3c979f98cc6ef095b41fc2bc512be6967
huangwuyi <792786297@qq.com>
2018-09-10 10:12:22 +0800

    增加了getCommonDate

--- androidbasehelper/src/main/java/com/cnhtc/cold/androidbasehelper/CommonDate.java
+++ androidbasehelper/src/main/java/com/cnhtc/cold/androidbasehelper/CommonDate.java
@@ -45,6 +45,44 @@ public class CommonDate {
         return simpleDateFormatPHP.format(date);
     }
 
+    public Date getCommonDate(String dateString){
+        SimpleDateFormat simpleDateFormatPHP=new SimpleDateFormat("MMM dd yyyy hh:mm:ss:SSSa", Locale.ENGLISH);
+        SimpleDateFormat simpleDateFormatYMDHMS = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
+        SimpleDateFormat simpleDateFormatYMDHMSWithCHN=new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+        SimpleDateFormat simpleDateFormatYMD = new SimpleDateFormat("yyyy-MM-dd");
+        Date date;
+        try {
+            date=simpleDateFormatPHP.parse(dateString);
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            date=simpleDateFormatYMDHMS.parse(dateString);
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            date=simpleDateFormatYMDHMSWithCHN.parse(dateString);
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            date=simpleDateFormatYMD.parse(dateString);
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        date=new Date();
+        return  date;
+    }
+
     public String getCommonDateTimeString(String dateString){
         SimpleDateFormat simpleDateFormatPHP=new SimpleDateFormat("MMM dd yyyy hh:mm:ss:SSSa", Locale.ENGLISH);
         SimpleDateFormat simpleDateFormatYMDHMS = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
